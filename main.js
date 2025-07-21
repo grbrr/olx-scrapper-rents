4f0c7b1ec243e4248a7c04e0adafeb56c37b55b2
grbrr <damianbecmer@gmail.com>
2025-07-21 14:14:08 +0200

    added hyperlink extraction and sorting by date to result array

--- main.js
+++ main.js
@@ -53,20 +53,38 @@ const request = async () => {
         for (let i = 0; i < costArr.length; i++) {
             costArr[i] = parseInt(costArr[i].replace(/ /g, ''));
         };
-        //console.log(costArr);
+        //console.log(costArr);    
 
-        console.log(titleArr.length, addressArr.length, dateArr.length, sizeArr.length, costArr.length);
+        //console.log(titleArr.length, addressArr.length, dateArr.length, sizeArr.length, costArr.length);
         let resultArr = [];
         for (let i = 0; i < titleArr.length; i++) {
+            // Pobierz link do ogłoszenia
+            let link = null;
+            const linkElem = $(getProperPath(settings.hyperlinkSelector)).eq(i);
+            if (linkElem.length) {
+                link = linkElem.attr('href');
+                if (link && link.startsWith('/')) {
+                    link = 'https://www.olx.pl' + link;
+                }
+            }
             resultArr[i] = {
-                number: i + 1,
+                //number: i + 1,
                 title: titleArr[i],
                 address: addressArr[i],
                 cost: costArr[i],
                 size: sizeArr[i],
-                date: dateArr[i]
+                date: dateArr[i],
+                link: link
             };
         };
+        resultArr.sort((a, b) => {
+            const isTodayA = a.date.startsWith('Dzisiaj o ');
+            const isTodayB = b.date.startsWith('Dzisiaj o ');
+            if (isTodayA && !isTodayB) return -1;
+            if (!isTodayA && isTodayB) return 1;
+            // Reverse alphabetic order for the rest
+            return b.date.localeCompare(a.date, 'pl');
+        });
         console.log(resultArr);
 
 
@@ -75,5 +93,12 @@ const request = async () => {
         console.error(`Error in otodom: ${e.message}`);
     };
 }
+
 console.log('starting script...');
 request();
+setInterval(() => {
+    console.log('restarting script...');
+    request();
+}, settings.interval * 1000);
+console.log(`Script will run every ${settings.interval} seconds.`);
+console.log('To stop the script, press Ctrl+C.');
